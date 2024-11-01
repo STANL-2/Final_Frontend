10e44598b74af9fdd10169d3b6a246d99f5a71e1
Bang1999 <bangdh1593@gmail.com>
2024-11-01 21:32:48 +0900

    feat: aside 생성(#2)

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <PageHeader />
 
   <main>
+    <PageAside />
     <RouterView />
   </main>
 
@@ -11,6 +12,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import PageHeader from './components/PageHeader.vue';
+import PageAside from './components/PageAside.vue';
 import { onMounted, onUnmounted } from 'vue';
 import DOMEventService from './services/DOMEventService';
 import { useRouter } from 'vue-router';
@@ -26,5 +28,7 @@ const userStore = useUserStore();
 main {
   flex-grow: 1;
   overflow-y: auto;
+  display: flex;
+  flex-direction: row;
 }
 </style>
