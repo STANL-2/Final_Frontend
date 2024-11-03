069ed216bc8b266bb783e3dc0887b3fbe5a8970d
Bang1999 <bangdh1593@gmail.com>
2024-11-04 00:31:26 +0900

    fix: 사이드바 수정(#7)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,20 @@
 export const asideMenu = [
-    { key: '0', label: '공지사항',
-        children: [{ key: '0-0', label: '공지 1' },
-            { key: '0-1', label: '고비 2' }]},
-    { key: '1', label: '기초정보',
-        children: [{ key: '1-0', label: '기초 1' },
-            { key: '1-1', label: '가조 2' }]},
-    { key: '2', label: '니ㅑ치', children: [{
-                key: '2-0', label: '비음 1' },
-            { key: '2-1', label: '내아 2' }]}
+    { key: '0', label: '공지사항', url: '/notice',
+        children: [
+            { key: '0-0', label: '공지 1', url: '/notice/1' },
+            { key: '0-1', label: '고비 2', url: '/notice/2' }
+        ]
+    },
+    { key: '1', label: '기초정보', url: '/basic-info',
+        children: [
+            { key: '1-0', label: '기초 1', url: '/basic-info/1' },
+            { key: '1-1', label: '가조 2', url: '/basic-info/2' }
+        ]
+    },
+    { key: '2', label: '로그인 페이지로 이동', url: '/login',
+        children: [
+            { key: '2-0', label: '로그인 페이지로 이동', url: '/login' },
+            { key: '2-1', label: '내아 2', url: '/niatchi/2' }
+        ]
+    }
 ];
