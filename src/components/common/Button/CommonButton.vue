9e09e5c9b783e17acb77f3fe3a38f656aba9ff42
yuhyejin <hj_eu@naver.com>
2024-11-05 00:13:10 +0900

    feat: 공통 버튼 컴포넌트 (#20)

---
+++ src/components/common/Button/CommonButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <Button 
+        :label="label" 
+        :icon="icon" 
+        :style="{ backgroundColor: color, color: textColor }" 
+        class="mt-4" 
+    />
+</template>
+
+<script setup>
+import Button from 'primevue/button';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        default: '',
+    },
+    color: {
+        type: String,
+        default: '#6360AB', // 기본 버튼 배경색
+    },
+    textColor: {
+        type: String,
+        default: '#ffffff', // 기본 버튼 텍스트 색상
+    },
+});
+</script>
+
+<style scoped>
+.p-button {
+    border: none;
+    border-radius: 6px;
+    font-size: 0.9rem;
+}
+</style>
--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -4,48 +4,39 @@
     </Dialog>
 </template>
 
-<script>
-import { defineProps, defineEmits, ref, watch } from 'vue';
+<script setup>
+import { ref, watch } from 'vue';
 import Dialog from 'primevue/dialog';
 
-export default {
-    components: { Dialog },
-    props: {
-        modelValue: {
-            type: Boolean,
-            required: true,
-        },
-        header: {
-            type: String,
-            default: 'Modal Header',
-        },
+const props = defineProps({
+    modelValue: {
+        type: Boolean,
+        required: true,
     },
-    emits: ['update:modelValue'],
-    setup(props, { emit }) {
-        const localVisible = ref(props.modelValue);
-
-        // 부모 컴포넌트의 modelValue가 바뀌면 localVisible도 업데이트
-        watch(
-            () => props.modelValue,
-            (newVal) => {
-                localVisible.value = newVal;
-            }
-        );
-
-        // localVisible이 바뀔 때마다 부모 컴포넌트에 상태를 업데이트
-        const hide = () => {
-            localVisible.value = false;
-            emit('update:modelValue', false);
-        };
-
-        watch(localVisible, (newVal) => {
-            emit('update:modelValue', newVal);
-        });
-
-        return {
-            localVisible,
-            hide,
-        };
+    header: {
+        type: String,
+        default: 'Modal Header',
     },
+});
+
+const emit = defineEmits(['update:modelValue']);
+const localVisible = ref(props.modelValue);
+
+// 부모 컴포넌트의 modelValue가 바뀌면 localVisible도 업데이트
+watch(
+    () => props.modelValue,
+    (newVal) => {
+        localVisible.value = newVal;
+    }
+);
+
+// localVisible이 바뀔 때마다 부모 컴포넌트에 상태를 업데이트
+const hide = () => {
+    localVisible.value = false;
+    emit('update:modelValue', false);
 };
+
+watch(localVisible, (newVal) => {
+    emit('update:modelValue', newVal);
+});
 </script>
