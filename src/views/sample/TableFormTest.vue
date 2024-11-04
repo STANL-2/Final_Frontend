7014dbce63bd990aeca061908107db938dafed7b
Bang1999 <bangdh1593@gmail.com>
2024-11-04 15:46:08 +0900

    fix: 바디 css 추가(#11)

--- src/views/sample/TableFormTest.vue
+++ src/views/sample/TableFormTest.vue
@@ -1,7 +1,33 @@
 <template>
-    <ViewForm/>
+    <div class="viewform">
+        <ViewForm :data="data"/>
+    </div>  
 </template>
 
 <script setup>
 import ViewForm from '@/components/common/ViewForm.vue';
+import { ref } from 'vue';
+
+// 더미 데이터 정의
+const data = ref([
+    {
+        label: '지점이름',
+        value: '지점 A',
+        secondLabel: '지점주소',
+        secondValue: '서울특별시 중구'
+    },
+    {
+        label: '지점 전화번호',
+        value: '010-1234-5678',
+        secondLabel: '본사여부',
+        secondValue: '본사'
+    }
+]);
 </script>
+
+<style scoped>
+.viewform{
+    display: flex;
+    width: 100%;
+}
+</style>
