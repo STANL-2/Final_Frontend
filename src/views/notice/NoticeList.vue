439f2ae4d1d2332c4e9c0f97e83704e29be06d0f
minseokKim6823 <alstjr6823@naver.com>
2024-11-24 17:15:13 +0900

    feat: 검색창 구현 완료(#78)

--- src/views/notice/NoticeList.vue
+++ src/views/notice/NoticeList.vue
@@ -1,13 +1,294 @@
 <template>
-    <div>
-        ReadOnly Notice(공지사항) 조회 페이지 입니다.
-    </div>
+    <PageLayout>
+        <!-- SearchForm -->
+        <div class="component-wrapper">
+            <SearchForm :fields="formFields" @open-modal="handleOpenModal" ref="searchFormRef" />
+        </div>
+
+        <div class="flex-row content-between">
+            <div class="list">전체목록</div>
+            <div class="flex-row items-center mb-s">
+                <div><CommonButton label="추가" icon="pi pi-plus" /></div>
+                <div class="ml-xs"><CommonButton label="엑셀다운" @click="exportCSV($event)" icon="pi pi-download" /></div>
+                <div class="ml-xs"><CommonButton label="인쇄" icon="pi pi-print" /></div>
+                <div class="ml-xs"><CommonButton label="초기화" icon="pi pi-refresh" color="#F1F1FD" textColor="#6360AB" /></div>
+            </div>
+        </div>
+
+        <!-- ViewTable -->
+        <div class="component-wrapper">
+            <ViewTable 
+                :headers="tableHeaders" 
+                :data="tableData" 
+                :loading="loading" 
+                :totalRecords="totalRecords" 
+                :rows="rows" 
+                :rowsPerPageOptions="[5, 10, 20, 50]"
+                :selectable="true" 
+                buttonLabel="조회" 
+                buttonHeader="상세조회"
+                :buttonAction="handleView" 
+                buttonField="code"
+                @page="onPage" 
+                @sort="onSort" 
+                @filter="onFilter" 
+            />
+
+            <ContractDetail
+            v-model="showDetailModal"
+            :showModal="showDetailModal"
+            :details="selectedDetail"
+            @close="showDetailModal = false"
+        />
+        </div>
+
+        <!-- 모달 -->
+        <Modal v-model="showModal" header="매장코드 검색" width="30rem" @confirm="confirmSelection" @cancel="resetModalState">
+            <div class="flex-row content-center mb-m">
+                <label class="mr-m">매장명: </label>
+                <!-- Enter 키 입력 시 searchStore 함수 호출 -->
+                <InputText type="text" v-model="searchQuery" @keyup.enter="searchStore" />
+                <button class="search-button" @click="searchStore">
+                    <span class="search-icon pi pi-search"></span>
+                </button>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th v-for="header in headers" :key="header">{{ header }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(row, index) in modalTableData" :key="index" @click="selectStore(row, index)"
+                        :class="{ selected: selectedRow === index }">
+                        <td>{{ row.매장코드 }}</td>
+                        <td>{{ row.매장명 }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <template #footer>
+                <CommonButton label="확인" @click="confirmSelection" />
+                <CommonButton label="취소" color="#ffffff" textColor="#6360AB" borderColor="#6360AB"
+                    @click="resetModalState" />
+            </template>
+        </Modal>
+    </PageLayout>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import PageLayout from '@/components/common/layouts/PageLayout.vue';
+import ViewTable from '@/components/common/ListTable.vue';
+import ContractDetail from '@/views/contract/ContractDetail.vue'
+import Modal from '@/components/common/Modal.vue';
+import SearchForm from '@/components/common/NoticeSearchForm.vue';
+import CommonButton from '@/components/common/Button/CommonButton.vue';
+import { $api } from '@/services/api/api';
 
+// 모달 테이블 값
+const headers = ['매장코드', '매장명'];
+const modalTableData = [
+    { 매장코드: 'A', '매장명': 50 },
+    { 매장코드: 'B', '매장명': 75 },
+];
+
+// SearchForm.vue 검색조건 값
+const formFields = [
+    [
+        // {
+        //     type: 'select',
+        //     label: '승인여부',
+        //     model: 'approvalStatus',
+        //     options: ['대기', '승인', '취소']
+        // },
+        // {
+        //     label: '고객 구분',
+        //     type: 'radio',
+        //     model: 'customerType',
+        //     options: ['개인', '법인'],
+        //     showDivider: false
+        // },
+        // {
+        //     label: '매장 검색',
+        //     type: 'inputWithButton',
+        //     model: 'storeSearch',
+        //     showDivider: false
+        // },
+        // {
+        //     label: '계약일',
+        //     type: 'calendar', // 쌍으로 처리
+        //     model: 'contractDate', // 시작과 종료를 모두 포함
+        //     showIcon: true,
+        //     manualInput: false,
+        // }
+        {
+            type: 'select',
+            label: '태그',
+            model: 'tag',
+            options: ['ALL','SERVICE','SALES','EVENT','HR']
+        },
+        {
+            type: 'select',
+            label: '분류',
+            model: 'classification',
+            options: ['IMPORTANT','NORMAL','WARNING','EVENT']
+        },
+    ],
+    [
+        {
+            label: '제목',
+            type: 'input',
+            model: 'noticeTitle',
+            showDivider: true
+        },
+        {
+            label: '작성자',
+            type: 'input',
+            model: 'noticeWriter',
+            showDivider: true
+        },
+        {
+            label: '조회기간',
+            type: 'calendar', // 쌍으로 처리
+            model: 'NoticeSearchDate', // 시작과 종료를 모두 포함
+            showIcon: true,
+            manualInput: false,
+        }
+    ]
+];
+
+const tableHeaders = [
+    { field: 'contractId', label: '계약서 번호', width: '15%' },
+    { field: 'title', label: '계약서명', width: '25%' },
+    { field: 'carName', label: '제품명', width: '13%' },
+    { field: 'customerName', label: '고객명', width: '13%' },
+    { field: 'customerClassifcation', label: '고객 구분', width: '10%' },
+    { field: 'customerPurchaseCondition', label: '구분 조건', width: '10%' },
+    { field: 'companyName', label: '고객 상호', width: '10%' },
+    { field: 'status', label: '승인 상태', width: '5%' }
+];
+
+// 상태 변수
+const tableData = ref([]); // 테이블 데이터
+const showDetailModal = ref(false); // 상세조회 모달 표시 여부
+const selectedDetail = ref(null); // 선택된 상세 데이터
+
+const totalRecords = ref(0); // 전체 데이터 개수
+const loading = ref(false); // 로딩 상태
+const rows = ref(5); // 페이지 당 행 수
+const first = ref(0); // 첫 번째 행 위치
+const filters = ref({}); // 필터
+const sortField = ref(null); // 정렬 필드
+const sortOrder = ref(null); // 정렬 순서
+
+function handleView(rowData) {
+    // 상세 데이터 설정 및 모달 열기
+    selectedDetail.value = rowData; // 클릭된 행 데이터 전달
+    showDetailModal.value = true;
+}
+
+// 데이터 로드 함수
+const loadData = async () => {
+    loading.value = true; // 로딩 시작
+    try {
+        const response = await $api.notice.get(
+            '',
+            '',
+        );
+        console.log('GET 요청 응답 결과');
+        console.log(response);
+
+        tableData.value = response.result.content; // 테이블 데이터 업데이트
+        totalRecords.value = response.result.totalElements; // 전체 데이터 개수 업데이트
+    } catch (error) {
+        console.error('데이터 로드 실패:', error);
+    } finally {
+        loading.value = false; // 로딩 종료
+    }
+};
+
+// 페이지 로드 시 데이터 로드
+onMounted(() => {
+    loadData();
+});
+
+// 페이지네이션 이벤트 처리
+function onPage(event) {
+    first.value = event.first;
+    rows.value = event.rows;
+    loadData(); // 데이터 다시 로드
+}
+
+// 정렬 이벤트 처리
+function onSort(event) {
+    sortField.value = event.sortField;
+    sortOrder.value = event.sortOrder;
+    loadData(); // 데이터 다시 로드
+}
+
+// 필터 이벤트 처리
+function onFilter(event) {
+    filters.value = event.filters;
+    loadData(); // 데이터 다시 로드
+}
+
+
+// 모달에서 쓰이는 값들
+const showModal = ref(false);
+const selectedRow = ref(null);
+const selectedCode = ref('');
+const searchFormRef = ref(null);
+const selectedFieldIndex = ref(null);
+const searchQuery = ref('');
+
+function handleOpenModal(fieldIndex) {
+    console.log(`Opening modal for field: ${fieldIndex}`);
+    selectedFieldIndex.value = fieldIndex; // 필드 인덱스 저장
+    showModal.value = true;
+}
+
+function selectStore(row, index) {
+    selectedRow.value = index;
+    selectedCode.value = row.매장코드;
+}
+
+function confirmSelection() {
+    if (selectedFieldIndex.value !== null) {
+        searchFormRef.value.updateFieldValue(selectedFieldIndex.value, selectedCode.value);
+    }
+    closeModal();
+}
+
+function resetModalState() {
+    closeModal();
+}
+
+function closeModal() {
+    showModal.value = false;
+    selectedRow.value = null;
+    selectedCode.value = '';
+}
+
+// 여기에 검색어 조회 API 로직 작성
+function searchStore() {
+    // 입력된 값 출력
+    console.log('검색어:', searchQuery.value);
+
+    // 추가적인 검색 로직을 여기에 추가할 수 있습니다.
+    if (searchQuery.value) {
+        alert(`검색어: ${searchQuery.value}`);
+    }
+    // 검색어 초기화
+    searchQuery.value = '';
+}
 </script>
 
 <style scoped>
-
+.list{
+    font-size: 1.5rem;
+    font-weight:bold;
+}
 </style>
+
+
