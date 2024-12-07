533dd2791d7d3ede5d7e0d3a4531a69e1cbb9215
Bang1999 <bangdh1593@gmail.com>
2024-12-08 02:06:37 +0900

    fix: 시스템 관리자 사이드바 해결(#222)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,11 +4,10 @@ export const EasideMenu = [
     {
         key: '0', label: '계약 관리',
         children: [
-            { key: '0-0', label: '계약서', url: '/commingsoon' },
-            { key: '0-1', label: '수주서', url: '/commingsoon' },
-            { key: '0-2', label: '판매내역', url: '/sales-history/Elist' },
-            { key: '0-3', label: '통계', url: '/sales-history/Echart' },
-
+            { key: '0-0', label: '계약서', url: '/contract/emlist' },
+            { key: '0-1', label: '수주서', url: '/order/emList' },
+            { key: '0-2', label: '판매내역 리스트', url: '/sales-history/Elist' },
+            { key: '0-3', label: '판매내역 통계', url: '/sales-history/Echart' }
         ]
     },
     {
@@ -27,7 +26,7 @@ export const EasideMenu = [
     {
         key: '3', label: '매장 관리',
         children: [
-            { key: '3-0', label: '영업매장', url: '/center/list' },
+            { key: '3-0', label: '영업 매장', url: '/center/list' },
             { key: '3-1', label: '제품', url: '/product/list' }
         ]
     },
@@ -41,7 +40,7 @@ export const EasideMenu = [
     {
         key: '5', label: '사원 관리',
         children: [
-            { key: '5-0', label: '사원 정보', url: '/employee/list' },
+            { key: '5-0', label: '사원 정보', url: '/employee/list' }
         ]
     }
 ];
@@ -55,7 +54,8 @@ export const AasideMenu = [
             { key: '0-0', label: '계약서', url: '/contract/Elist' },
             { key: '0-1', label: '수주서', url: '/order/adList' },
             { key: '0-2', label: '발주서', url: '/purchase-order/adlist' },
-            { key: '0-3', label: '본인 판매내역', url: '/sales-history/Elist' }
+            { key: '0-3', label: '판매내역 리스트', url: '/sales-history/Elist' },
+            { key: '0-4', label: '판매내역 통계', url: '/sales-history/Echart' }
         ]
     },
     {
@@ -90,8 +90,7 @@ export const AasideMenu = [
     {
         key: '5', label: '사원 관리',
         children: [
-            { key: '5-0', label: '사원 정보', url: '/employee/list' },
-            { key: '5-1', label: '평가서', url: '/evaluation/list' }
+            { key: '5-0', label: '사원 정보', url: '/employee/list' }
         ]
     }
 ];
@@ -101,9 +100,9 @@ export const DasideMenu = [
     {
         key: '0', label: '계약 관리',
         children: [
-            { key: '0-0', label: '계약서', url: '/contract/list' },
-            { key: '0-1', label: '수주서', url: '/order/list' },
-            { key: '0-2', label: '발주서', url: '/purchase-order/list' },
+            { key: '0-0', label: '계약서', url: '/contract/dlist' },
+            { key: '0-1', label: '수주서', url: '/order/dlist' },
+            { key: '0-2', label: '발주서', url: '/purchase-order/ddlist' },
         ]
     },
     {
@@ -138,8 +137,7 @@ export const DasideMenu = [
     {
         key: '5', label: '사원 관리',
         children: [
-            { key: '5-0', label: '사원 정보', url: '/employee/list' },
-            { key: '5-1', label: '평가서', url: '/evaluation/list' }
+            { key: '5-0', label: '사원 정보', url: '/employee/list' }
         ]
     }
 ];
@@ -147,50 +145,153 @@ export const DasideMenu = [
 
 export const GasideMenu = [
     {
-        key: '0', label: '계약 관리',
+        key: '0', label: '영업 사원', // depth1
         children: [
-            { key: '0-0', label: '계약서', url: '/contract/list' },
-            { key: '0-1', label: '수주서', url: '/order/list' },
-            { key: '0-2', label: '발주서', url: '/purchase-order/list' },
+            {
+                key: '0-0', label: '계약 관리', // depth2
+                children: [
+                    { key: '0-0-0', label: '계약서', url: '/contract/emlist' }, // depth3
+                    { key: '0-0-1', label: '수주서', url: '/order/emList' },
+                    { key: '0-0-2', label: '판매내역 리스트', url: '/sales-history/Elist' },
+                    { key: '0-0-3', label: '판매내역 통계', url: '/sales-history/Echart' }
+                ]
+            },
+            {
+                key: '0-1', label: '고객 관리',
+                children: [
+                    { key: '0-1-0', label: '고객 정보', url: '/customer/list' },
+                    { key: '0-1-1', label: '문제사항', url: '/problem/list' }
+                ]
+            },
+            {
+                key: '0-2', label: '일정 관리',
+                children: [
+                    { key: '0-2-0', label: '일정', url: '/schedule' }
+                ]
+            },
+            {
+                key: '0-3', label: '매장 관리',
+                children: [
+                    { key: '0-3-0', label: '영업 매장', url: '/center/list' },
+                    { key: '0-3-1', label: '제품', url: '/product/list' }
+                ]
+            },
+            {
+                key: '0-4', label: '공지 및 프로모션',
+                children: [
+                    { key: '0-4-0', label: '공지사항', url: '/notice/list' },
+                    { key: '0-4-1', label: '프로모션', url: '/promotion/list' }
+                ]
+            },
+            {
+                key: '0-5', label: '사원 관리',
+                children: [
+                    { key: '0-5-0', label: '사원 정보', url: '/employee/list' }
+                ]
+            }
         ]
     },
     {
-        key: '1', label: '고객 관리',
+        key: '1', label: '영업 관리자',
         children: [
-            { key: '1-0', label: '고객 정보', url: '/customer/list' },
-            { key: '1-1', label: '문제사항', url: '/problem/list' }
+            {
+                key: '1-0', label: '계약 관리',
+                children: [
+                    { key: '1-0-0', label: '계약서', url: '/contract/Elist' },
+                    { key: '1-0-1', label: '수주서', url: '/order/adList' },
+                    { key: '1-0-2', label: '발주서', url: '/purchase-order/adlist' },
+                    { key: '1-0-3', label: '판매내역 리스트', url: '/sales-history/Elist' },
+                    { key: '1-0-4', label: '판매내역 통계', url: '/sales-history/Echart' }
+                ]
+            },
+            {
+                key: '1-1', label: '고객 관리',
+                children: [
+                    { key: '1-1-0', label: '고객 정보', url: '/customer/list' },
+                    { key: '1-1-1', label: '문제사항', url: '/problem/list' }
+                ]
+            },
+            {
+                key: '1-2', label: '일정 관리',
+                children: [
+                    { key: '1-2-0', label: '일정', url: '/schedule' }
+                ]
+            },
+            {
+                key: '1-3', label: '매장 관리',
+                children: [
+                    { key: '1-3-0', label: '영업 매장', url: '/center/list' },
+                    { key: '1-3-1', label: '제품', url: '/product/list' },
+                    { key: '1-3-2', label: '판매내역 리스트', url: '/sales-history/list' },
+                    { key: '1-3-3', label: '판매내역 통계', url: '/sales-history/chart' }
+                ]
+            },
+            {
+                key: '1-4', label: '공지 및 프로모션',
+                children: [
+                    { key: '1-4-0', label: '공지사항', url: '/notice/list' },
+                    { key: '1-4-1', label: '프로모션', url: '/promotion/list' }
+                ]
+            },
+            {
+                key: '1-5', label: '사원 관리',
+                children: [
+                    { key: '1-5-0', label: '사원 정보', url: '/employee/list' }
+                ]
+            }
         ]
     },
     {
-        key: '2', label: '일정 관리',
+        key: '2', label: '영업 담당자',
         children: [
-            { key: '2-0', label: '일정', url: '/schedule' }
-        ]
-    },
-    {
-        key: '3', label: '매장 관리',
-        children: [
-            { key: '3-0', label: '영업매장', url: '/center/list' },
-            { key: '3-1', label: '제품', url: '/product/list' },
-            { key: '3-2', label: '판매내역 리스트', url: '/sales-history/list' },
-            { key: '3-3', label: '판매내역 통계', url: '/sales-history/chart' }
-        ]
-    },
-    {
-        key: '4', label: '공지 및 프로모션',
-        children: [
-            { key: '4-0', label: '공지사항', url: '/notice/Elist' },
-            { key: '4-1', label: '프로모션', url: '/promotion/Elist' }
-        ]
-    },
-    {
-        key: '5', label: '사원 관리',
-        children: [
-            { key: '5-0', label: '사원 정보', url: '/employee/list' },
-            { key: '5-1', label: '평가서', url: '/evaluation/list' }
+            {
+                key: '2-0', label: '계약 관리',
+                children: [
+                    { key: '2-0-0', label: '계약서', url: '/contract/dlist' },
+                    { key: '2-0-1', label: '수주서', url: '/order/dlist' },
+                    { key: '2-0-2', label: '발주서', url: '/purchase-order/ddlist' }
+                ]
+            },
+            {
+                key: '2-1', label: '고객 관리',
+                children: [
+                    { key: '2-1-0', label: '고객 정보', url: '/customer/list' },
+                    { key: '2-1-1', label: '문제사항', url: '/problem/list' }
+                ]
+            },
+            {
+                key: '2-2', label: '일정 관리',
+                children: [
+                    { key: '2-2-0', label: '일정', url: '/schedule' }
+                ]
+            },
+            {
+                key: '2-3', label: '매장 관리',
+                children: [
+                    { key: '2-3-0', label: '영업 매장', url: '/center/list' },
+                    { key: '2-3-1', label: '제품', url: '/product/list' },
+                    { key: '2-3-2', label: '판매내역 리스트', url: '/sales-history/list' },
+                    { key: '2-3-3', label: '판매내역 통계', url: '/sales-history/chart' }
+                ]
+            },
+            {
+                key: '2-4', label: '공지 및 프로모션',
+                children: [
+                    { key: '2-4-0', label: '공지사항', url: '/notice/Elist' },
+                    { key: '2-4-1', label: '프로모션', url: '/promotion/Elist' }
+                ]
+            },
+            {
+                key: '2-5', label: '사원 관리',
+                children: [
+                    { key: '2-5-0', label: '사원 정보', url: '/employee/list' }
+                ]
+            }
         ]
     }
 ];
+
+
 
 export const asideMenu = ref([]);
 
