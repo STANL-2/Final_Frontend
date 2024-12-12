e6268f9514c86727bf6dd03adb219bd6818e06f2
Bang1999 <bangdh1593@gmail.com>
2024-12-12 10:42:48 +0900

    고객 승인 수정 APPROVED

--- src/views/customer/CustomerDetail.vue
+++ src/views/customer/CustomerDetail.vue
@@ -197,7 +197,7 @@ function getStatusLabel(status) {
     switch (status) {
         case "WAIT":
             return "대기";
-        case "APPROVE":
+        case "APPROVED":
             return "승인";
         case "CANCEL":
             return "취소";
@@ -211,7 +211,7 @@ function getCustomTagClass(status) {
     switch (status) {
         case "WAIT":
             return "success"; // 노란색
-        case "APPROVE":
+        case "APPROVED":
             return "info"; // 초록색
         case "CANCEL":
             return "danger"; // 빨간색
